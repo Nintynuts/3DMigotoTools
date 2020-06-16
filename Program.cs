c90d2850626820900cba4842e4ee41979b3326b9
Lewis Smith <nintynuts@gmail.com>
2020-06-16 14:49:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Migoto.Log.Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new StreamReader(args[0]);
+
+            var parser = new Parser(file);
+
+            parser.Parse();
+        }
+    }
+}
